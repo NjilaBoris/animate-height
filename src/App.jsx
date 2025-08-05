10b34475c28afabf2770ff8004545746fb31b1f1
NjilaBoris <njilaboris2@gmail.com>
2025-08-05 03:27:05 +0100

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { motion } from "motion/react";
+import useMeasure from "react-use-measure";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [showExtraContent, setShowExtraContent] = useState(false);
+  const [ref, bounds] = useMeasure();
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="wrapper">
+      <button className="button" onClick={() => setShowExtraContent((b) => !b)}>
+        Toggle height
+      </button>
+      <motion.div animate={{ height: bounds.height }} className="element">
+        <div className="inner" ref={ref}>
+          <h1>Fake Family Drawer</h1>
+          <p>
+            This is a fake family drawer. Animating height is tricky, but
+            satisfying when it works.
+          </p>
+          {showExtraContent ? (
+            <p>This extra content will change the height of the drawer.</p>
+          ) : null}
+        </div>
+      </motion.div>
+    </div>
+  );
+};
 
-export default App
+export default App;
